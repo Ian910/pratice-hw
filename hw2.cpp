e6f20289119a865fe3a6897df41a81ba8b8fbbd5
Ian910 <66122637+Ian910@users.noreply.github.com>
2020-07-16 09:59:04 +0800

    Update hw2.cpp

--- hw2.cpp
+++ hw2.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
-
 int main()
-{
-	 
+{ 
 	int H,S,i,j;
 	cout << "Please input the odd height of the rhombus(3~15):";
 	cin >> H;
@@ -45,12 +43,57 @@ int main()
 		{
 			cout << "You enter the wrong number,please input again!" << endl;
 			cin >> H;
-		}
-		  
-		
+		}	  	
 	}
-
-
+	system("pause");
+	return 0;
+}
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i, j, n, s;
+	printf("Please input the odd height of the rhombus(3~15):");
+	scanf_s("%d", &n);
+	while (n != -1)
+	{
+		if (n <= 15 && n > 1 && n % 2 != 0)
+		{
+			s = (n - 1) / 2;
+			for (i = 1; i <= s; i++)
+			{
+				for (j = s + 1 - i; j > 0; j--)
+				{
+					printf(" ");
+				}
+				for (j = 1; j <= 2 * i - 1; j++)
+				{
+					printf("*");
+				}
+				printf("\n");
+			}
+			for (i = 1; i <= s + 1; i++)
+			{
+				for (j = 1; j <= i - 1; j++)
+				{
+					printf(" ");
+				}
+				for (j = 1; j <= n - 2 * i + 2; j++)
+				{
+					printf("*");
+				}
+				printf("\n");
+			}
+			printf("Please input the odd height of the rhombus(3~15):");
+			scanf_s("%d", &n);
+		}
+		else
+		{
+			printf("You enter the wrong number,please input again!\n");
+			scanf_s("%d", &n);
+		}
+	}
 	system("pause");
 	return 0;
 }
